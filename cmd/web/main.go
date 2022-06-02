c2d779987f7fcffbd4cacff73dafbec9834ed810
Caronise <lucas.betancourt@hotmail.com>
2022-06-01 23:10:17 -0400

    Added flag variable for -port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	port := flag.String("port", ":8181", "Port to run the server on.")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -16,7 +20,7 @@ func main() {
 	mux.HandleFunc("/zettel/view", zettelView)
 	mux.HandleFunc("/zettel/create", zettelCreate)
 
-	log.Println("Starting server on port :8181")
-	err := http.ListenAndServe(":8181", mux)
+	log.Printf("Starting server on %s", *port)
+	err := http.ListenAndServe(*port, mux)
 	log.Fatal(err)
 }
