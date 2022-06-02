b96c859fdd24d59472de55305a4e8f3e9c0a4a87
Caronise <lucas.betancourt@hotmail.com>
2022-06-01 22:45:27 -0400

    using fileserver to handle static files. Fixed a typo in base.tmpl.html

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", fileServer)
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/zettel/view", zettelView)
 	mux.HandleFunc("/zettel/create", zettelCreate)
